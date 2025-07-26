18fdbb5c3c0da20079a7a87b40224d2eef8ed8a2
yaaabs <brianyabut02@gmail.com>
2025-07-26 08:55:53 +0800

    Added basic design for login and regis

--- app.py
+++ app.py
@@ -277,6 +277,6 @@ def index():
 if __name__ == '__main__':
     import os
     port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host='0.0.0.0', port=port)
+    socketio.run(app, host='0.0.0.0', port=port, debug=True)
 
 
